0be856e926dc9e5c79ef46c684dd12324864269e
“Sanika <sanika.gadgil@kcl.ac.uk>
2024-12-11 14:53:58 +0000

    Merge conflict reolved part 2

--- tutorials/forms/admin_forms.py
+++ tutorials/forms/admin_forms.py
@@ -2,13 +2,10 @@ from django import forms
 from tutorials.models.lesson_model import Lesson
 from tutorials.models.booking_model import Booking
 from tutorials.models.user_models import User
-<<<<<<< HEAD
 from tutorials.models.admin_model import Admin
 
 #TODO: Not implemented yet
-=======
 
->>>>>>> d180e004bb72890fc15e755d513db3254ae770bd
 class UpdateLessonForm(forms.ModelForm):
     "Form to edit a lesson"
     class Meta:
@@ -25,9 +22,5 @@ class UpdateUserForm(forms.ModelForm):
     "Form to edit a user"
     class Meta:
         model = User
-<<<<<<< HEAD
-        fields = ['username', 'first_name', 'last_name', 'email']
 
-=======
         fields = ['username', 'first_name', 'last_name', 'email']
->>>>>>> d180e004bb72890fc15e755d513db3254ae770bd
